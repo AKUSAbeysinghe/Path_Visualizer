8da9878d30a921a3e7694842b5eb22a719e15894
A.K.U.S Abeysinghe <umeshsewwandi420@gmail.com>
2024-12-25 22:49:39 +0530

    first commit

--- src/context/PathfindingContext.ts
+++ src/context/PathfindingContext.ts
@@ -1,8 +1,14 @@
-import { AlgorithmType, MazeType } from "../utils/types";
+import { createContext } from "react";
+import { AlgorithmType, GridType, MazeType } from "../utils/types";
 
 interface PathfindingContextInterface {
     algorithm: AlgorithmType;
     setAlgorithm: (algorithm: AlgorithmType) => void;
     maze: MazeType;
-    
+    grid: GridType;
+    setGrid: (grid: GridType) => void;
+    isGraphVisualized: boolean;
+  setIsGraphVisualized: (isGraphVisualized: boolean) => void;
   }
+
+  export const PathfindingContext = createContext<PathfindingContextInterface | undefined>(undefined);
