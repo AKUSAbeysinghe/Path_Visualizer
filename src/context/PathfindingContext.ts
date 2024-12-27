aef63a7af7e8b9cf6a09abf6802fc5eaa23eae8b
A.K.U.S Abeysinghe <umeshsewwandi420@gmail.com>
2024-12-27 16:12:26 +0530

    second

--- src/context/PathfindingContext.ts
+++ src/context/PathfindingContext.ts
@@ -1,5 +1,7 @@
-import { createContext } from "react";
+import { createContext, ReactNode, useState } from "react";
 import { AlgorithmType, GridType, MazeType } from "../utils/types";
+import { createGrid } from "../utils/helpers";
+import { END_TILE_CONFIGURATION, START_TILE_CONFIGURATION } from "../utils/constants";
 
 interface PathfindingContextInterface {
     algorithm: AlgorithmType;
@@ -12,3 +14,11 @@ interface PathfindingContextInterface {
   }
 
   export const PathfindingContext = createContext<PathfindingContextInterface | undefined>(undefined);
+
+  export const PathfindingProvider = ({ children }: { children: ReactNode }) => {
+    const [algorithm, setAlgorithm] = useState<AlgorithmType>("BFS");
+    const [maze, setMaze] = useState<MazeType>("NONE");
+    const [grid, setGrid] = useState<GridType>(
+      createGrid(START_TILE_CONFIGURATION, END_TILE_CONFIGURATION)
+    );
+  }
